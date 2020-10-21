155c5459a4a72d68aa4353279cd0f8a1cf8d948d
MehdiZahwa <zahouani.mehdi06@gmail.com>
2020-10-21 16:57:33 +0100

    chore: refactor auth-guards

--- src/app/services/authentication/auth-guard.service.ts
+++ src/app/services/authentication/auth-guard.service.ts
@@ -9,12 +9,11 @@ export class AuthGuardService {
   constructor(private router: Router, private authService: AuthService) {}
 
   canActivate() {
-    // Check to see if a user has a valid token
+    // Check to see if a user is logged in
     if (this.authService.isLoggedIn) {
       // If they do, return true and allow the user to load app
       return true;
     }
-
     // If not, they redirect them to 404 page
     this.router.navigate(['/none']);
     return false;
